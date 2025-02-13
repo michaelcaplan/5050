02e9692dc98eb09ebeec3811a3d78b5bbef8e8ef
Mike Caplan <michael.caplan@henryscheinone.com>
2025-02-13 13:04:30 -0400

    stub

--- src/index.php
+++ src/index.php
@@ -6,23 +6,28 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 
 // This Appwrite function will be executed every time your function is triggered
 return function ($context) {
-    // You can use the Appwrite SDK to interact with other services
-    // For this example, we're using the Users service
+
+    try {
+        // You can use the Appwrite SDK to interact with other services
+        // For this example, we're using the Users service
 //    $client = new Client();
 //    $client
 //        ->setEndpoint(getenv('APPWRITE_FUNCTION_API_ENDPOINT'))
 //        ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
 //        ->setKey($context->req->headers['x-appwrite-key']);
 
-    $context->log('Total users: Hi!');
+        $context->log('Total users: Hi!');
 
 
-    if ($context->req->path === '/stripe') {
+        if ($context->req->path === '/stripe') {
+            $context->log(json_encode($context->req->bodyJson));
 
-        $context->log(json_encode($context->req->bodyJson));
-
-        return $context->res->json('Thanks Stripe, buddy, old pal.');
+            return $context->res->json('Thanks Stripe, buddy, old pal.');
+        }
+    } catch (Exception $e) {
+        $context->log((string) $e);
     }
+
 
     return $context->res->json('Nope');
 };
