345262f640ae7a2f6d7c8459eb9ae0390f45dcf5
Mike Caplan <michael.caplan@henryscheinone.com>
2025-02-13 12:58:39 -0400

    stub

--- src/index.php
+++ src/index.php
@@ -2,17 +2,17 @@
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
-use Appwrite\Client;
+//use Appwrite\Client;
 
 // This Appwrite function will be executed every time your function is triggered
 return function ($context) {
     // You can use the Appwrite SDK to interact with other services
     // For this example, we're using the Users service
-    $client = new Client();
-    $client
-        ->setEndpoint(getenv('APPWRITE_FUNCTION_API_ENDPOINT'))
-        ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
-        ->setKey($context->req->headers['x-appwrite-key']);
+//    $client = new Client();
+//    $client
+//        ->setEndpoint(getenv('APPWRITE_FUNCTION_API_ENDPOINT'))
+//        ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
+//        ->setKey($context->req->headers['x-appwrite-key']);
 
     $context->log('Total users: Hi!');
 
