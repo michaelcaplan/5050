b3fe955c6f9452bd9878d13530aeb66cceedc489
Mike Caplan <michael.caplan@henryscheinone.com>
2025-02-13 12:56:19 -0400

    stub

--- src/index.php
+++ src/index.php
@@ -3,7 +3,6 @@
 require_once(__DIR__ . '/../vendor/autoload.php');
 
 use Appwrite\Client;
-use Appwrite\Services\Users;
 
 // This Appwrite function will be executed every time your function is triggered
 return function ($context) {
@@ -14,16 +13,8 @@ return function ($context) {
         ->setEndpoint(getenv('APPWRITE_FUNCTION_API_ENDPOINT'))
         ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
         ->setKey($context->req->headers['x-appwrite-key']);
-    $users = new Users($client);
 
-    try {
-        $response = $users->list();
-        // Log messages and errors to the Appwrite Console
-        // These logs won't be seen by your end users
-        $context->log('Total users: ' . $response['total']);
-    } catch(Throwable $error) {
-        $context->error('Could not list users: ' . $error->getMessage());
-    }
+    $context->log('Total users: Hi!');
 
 
     if ($context->req->path === '/stripe') {
